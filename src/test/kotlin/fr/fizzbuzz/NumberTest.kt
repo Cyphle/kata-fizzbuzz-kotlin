3cd83e7a87e5eca926dc3b65d1afb575dd45ea33
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-12 20:51:05 +0200

    Return true when number is multiple of 3

--- src/test/kotlin/fr/fizzbuzz/NumberTest.kt
+++ src/test/kotlin/fr/fizzbuzz/NumberTest.kt
@@ -7,18 +7,24 @@ class NumberTest {
   @Test
   fun `should return false if number is 0 and tested against multiple of 3`() {
     val number = Number()
-    assertThat(number.isMultipleOfThree()).isFalse();
+    assertThat(number.isMultipleOfThree()).isFalse()
   }
 
   @Test
   fun `should return true if number is 3 and tested against multiple of 3`() {
     val number = Number(3)
-    assertThat(number.isMultipleOfThree()).isTrue();
+    assertThat(number.isMultipleOfThree()).isTrue()
   }
 
   @Test
   fun `should return false if number is not a multiple of 3 and tested against a multiple of 3`() {
     val number = Number(4)
-    assertThat(number.isMultipleOfThree()).isFalse();
+    assertThat(number.isMultipleOfThree()).isFalse()
+  }
+
+  @Test
+  fun `should return true if number is a multiple of 3`() {
+    val number = Number(15)
+    assertThat(number.isMultipleOfThree()).isTrue()
   }
 }
