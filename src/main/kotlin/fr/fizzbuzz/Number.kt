5e2cf8187d2ecc3829b8f20d9d85c057743fc9dc
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-12 21:02:34 +0200

    Replace fun by isMultipleOf(multiplicity: Int)

--- src/main/kotlin/fr/fizzbuzz/Number.kt
+++ src/main/kotlin/fr/fizzbuzz/Number.kt
@@ -1,12 +1,5 @@
 package fr.fizzbuzz
 
 class Number(val value: Int = 0) {
-  fun isMultipleOfThree(): Boolean {
-    return when {
-      value != 0 && value%3 == 0 -> true
-      else -> false
-    }
-  }
-
-  fun isMultipleOfFive(): Boolean = value != 0 && value%5 == 0
+  fun isMultipleOf(multiplicity: Int): Boolean = value != 0 && value%multiplicity == 0
 }
--- src/test/kotlin/fr/fizzbuzz/NumberTest.kt
+++ src/test/kotlin/fr/fizzbuzz/NumberTest.kt
@@ -7,36 +7,36 @@ class NumberTest {
   @Test
   fun `should return false if number is 0 and tested against multiple of 3`() {
     val number = Number()
-    assertThat(number.isMultipleOfThree()).isFalse()
+    assertThat(number.isMultipleOf(3)).isFalse()
   }
 
   @Test
   fun `should return true if number is 3 and tested against multiple of 3`() {
     val number = Number(3)
-    assertThat(number.isMultipleOfThree()).isTrue()
+    assertThat(number.isMultipleOf(3)).isTrue()
   }
 
   @Test
   fun `should return false if number is not a multiple of 3 and tested against a multiple of 3`() {
     val number = Number(4)
-    assertThat(number.isMultipleOfThree()).isFalse()
+    assertThat(number.isMultipleOf(3)).isFalse()
   }
 
   @Test
   fun `should return true if number is a multiple of 3`() {
     val number = Number(15)
-    assertThat(number.isMultipleOfThree()).isTrue()
+    assertThat(number.isMultipleOf(3)).isTrue()
   }
 
   @Test
   fun `should return false if number is not a multiple of 5`() {
     val number = Number(4)
-    assertThat(number.isMultipleOfFive()).isFalse()
+    assertThat(number.isMultipleOf(5)).isFalse()
   }
 
   @Test
   fun `should return true if multiple of 5`() {
     val number = Number(15)
-    assertThat(number.isMultipleOfFive()).isTrue()
+    assertThat(number.isMultipleOf(5)).isTrue()
   }
 }
